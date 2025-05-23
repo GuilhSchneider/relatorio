c5300fa0ad2cd00c24a0e643036fc6f412c44f86
GuilhSchneider <160530132+GuilhSchneider@users.noreply.github.com>
2025-05-23 13:17:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function gerar(pdf = false, print = false) {
+    for (let i = 1; i <= 4; i++)
+        document.getElementById(`relatorio${i}`).textContent = document.getElementById(`input${i}`).value;
+
+    const relatorio = document.getElementById('relatorio');
+    relatorio.style.visibility = 'visible';
+    relatorio.style.position = 'static';
+
+
+    if (pdf) html2pdf().from(relatorio).save();
+    else if (print) window.print();
+}
